875fa6f1b96491a6d2fec9f12805c45186bd7a7d
gabriel soares <gabrielsantossoares18@gmail.com>
2022-11-13 16:54:13 -0300

    up

---
+++ series_vista/resources/views/series/create.blade.php
@@ -0,0 +1,11 @@
+<x-layout title='Nova Série'>
+
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+    <div class="mb-3">
+        <label for="nome" class="form-label">nome:</label>
+        <input type="text" name="nome" id="nome" class="form-control">
+    </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
+</x-layout>
