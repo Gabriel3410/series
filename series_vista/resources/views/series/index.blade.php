332abed0b83e4eeda9aebb3d1b25efc53b191590
gabriel soares <110830279+Gabriel3410@users.noreply.github.com>
2022-12-08 17:37:42 -0300

    Delete series_vista directory

--- series_vista/resources/views/series/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout title='Séries'>
-    <a href="{{route('series.create')}}" class="btn btn-dark mb-2">Adicionar uma nova série!</a>
-
-    <ul class="list-group">
-        @foreach($series as $serie)
-            <li class="list-group-item"> {{$serie->nome}} </li>
-        @endforeach
-    </ul>
-</x-layout>
